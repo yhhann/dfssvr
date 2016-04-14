3e999a9bf63f8be01a10341067344b22a5cbe5e7
hanyh <hanyh@jingoal.com>
2016-04-13 20:24:03 -0700

    refine for code review for CL 101086.
    cl=101108

--- src/jingoal/dfs/metadata/event_test.go
+++ src/jingoal/dfs/metadata/event_test.go
@@ -8,93 +8,8 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-const (
-	dbUri  = "mongodb://192.168.55.193:27017?maxPoolSize=30"
-	dbName = "shard"
-)
-
-func TestSegment(t *testing.T) {
-	c := Segment{Domain: 200, NormalServer: "shard1", MigrateServer: "shard200"}
-
-	sop, err := NewMongoMetaOp(dbName, dbUri)
-	if err != nil {
-		t.Errorf("init chunk col error: %v\n", err)
-	}
-
-	err = sop.SaveSegment(&c)
-	if err != nil {
-		t.Errorf("save chunk error: %v\n", err)
-	}
-
-	cf, err := sop.LookupSegmentByDomain(200)
-	if err != nil {
-		t.Errorf("find chunk error: %v\n", err)
-	}
-	if cf.Domain != c.Domain {
-		t.Errorf("find chunk error: not expected.")
-	}
-
-	err = sop.RemoveSegment(200)
-	if err != nil {
-		t.Errorf("remove chunk error: %v\n", err)
-	}
-}
-
-func TestFindAllSegmentsOrderByDomain(t *testing.T) {
-	sop, err := NewMongoMetaOp(dbName, dbUri)
-	if err != nil {
-		t.Errorf("init chunk col error: %v\n", err)
-	}
-
-	chunks := sop.FindAllSegmentsOrderByDomain()
-	if len(chunks) != 4 {
-		t.Errorf("FindAllSegmentsOrderByDomain length error: %v\n", len(chunks))
-	}
-}
-
-func TestShard(t *testing.T) {
-	sop, err := NewMongoMetaOp(dbName, dbUri)
-	if err != nil {
-		t.Errorf("init server col error: %v\n", err)
-	}
-
-	s, err := sop.LookupShardByName("gluster1")
-	if s == nil || err != nil {
-		t.Errorf("find server shard1 error")
-	}
-
-	s, err = sop.LookupShardByName("shard-not-exist")
-	if s != nil || err == nil {
-		t.Errorf("find server not-exist error")
-	}
-}
-
-func TestFindAllShards(t *testing.T) {
-	sop, err := NewMongoMetaOp(dbName, dbUri)
-	if err != nil {
-		t.Errorf("init server col error: %v\n", err)
-	}
-
-	shards := sop.FindAllShards()
-	if len(shards) != 5 {
-		t.Errorf("find all servers error: %v\n", err)
-	}
-
-	found := false
-	for _, shard := range shards {
-		if shard.ShdType == DegradeServer {
-			found = true
-			break
-		}
-	}
-
-	if !found {
-		t.Errorf("degrade server not found.")
-	}
-}
-
 func TestEvent(t *testing.T) {
-	sop, err := NewMongoMetaOp(dbName, dbUri)
+	sop, err := NewEventOp(dbName, dbUri)
 	if err != nil {
 		t.Errorf("init event col error %v\n", err)
 	}
@@ -119,9 +34,9 @@ func TestEvent(t *testing.T) {
 
 func TestGetEvents(t *testing.T) {
 	fid := fmt.Sprintf("%x", string(bson.NewObjectId()))
-	sop, err := NewMongoMetaOp(dbName, dbUri)
+	sop, err := NewEventOp(dbName, dbUri)
 	if err != nil {
-		t.Errorf("NewMongoMetaOp() error %v\n", err)
+		t.Errorf("NewEventOp() error %v\n", err)
 	}
 
 	start := time.Now().Unix()
@@ -152,7 +67,7 @@ func TestGetEvents(t *testing.T) {
 		cnt++
 	}
 	iter.Close()
-	if cnt != 0 {
+	if cnt != 3 {
 		t.Errorf("GetEvent error, %d", cnt)
 	}
 
@@ -161,7 +76,7 @@ func TestGetEvents(t *testing.T) {
 	for iter.Next(&e) {
 		cnt++
 	}
-	if cnt != 0 {
+	if cnt != 3 {
 		t.Errorf("GetEvent error, %d", cnt)
 	}
 
@@ -170,7 +85,7 @@ func TestGetEvents(t *testing.T) {
 	for iter.Next(&e) {
 		cnt++
 	}
-	if cnt != 0 {
+	if cnt != 2 {
 		t.Errorf("GetEvent error, %d", cnt)
 	}
 
@@ -195,8 +110,4 @@ func TestGetEvents(t *testing.T) {
 	sop.RemoveEvent(id1)
 	sop.RemoveEvent(id2)
 	sop.RemoveEvent(id3)
-}
-
-func toString(o interface{}) string {
-	return fmt.Sprintf("%v", o)
 }
