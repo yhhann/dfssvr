2109981517ad5e02b54530b200bc8f623acbce23
hanyh <hanyh@jingoal.com>
2017-07-31 00:30:54 -0700

    Add interface for the new file meta package.
    original_cl=117017
    submit_cl=117018

---
+++ meta/meta.go
@@ -0,0 +1,45 @@
+package meta
+
+import "time"
+
+const (
+	EntityNone EntityType = iota
+	EntityGlusterFS
+	EntityGridFS
+	EntitySeaweedFS
+)
+
+// Type of file entity.
+type EntityType uint8
+
+// File represents the metadata of a file.
+type File struct {
+	Id         string
+	Biz        string
+	Name       string
+	Md5        string
+	UserId     string
+	Domain     int64
+	Size       int64
+	ChunkSize  int
+	UploadDate time.Time
+	ExtAttr    map[string]string
+	Type       EntityType
+}
+
+type FileMetaOp interface {
+	// Find looks up the metadata of a file by its fid.
+	Find(fid string) (*File, error)
+
+	// Save saves the metadata of a file.
+	Save(*File) error
+
+	// FindByMd5 looks up the metadata of a file by its md5 and domain.
+	FineByMd5(md5 string, domain int64) (*File, error)
+
+	// DuplicateWithId duplicates a given file by its fid.
+	DuplicateWithId(fid string, did string, createDate time.Timer) (string, error)
+
+	// Delete deletes a file.
+	Delete(find string) (tobeDeleted bool, entityIdToBeDeleted string, err error)
+}
